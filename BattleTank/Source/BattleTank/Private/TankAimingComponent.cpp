fcdfc7f5fed77e416eebb411db80243d32f265aa
DESKTOP-S1IQFSU\mkolisz11 <mkolisz11@gmail.com>
2019-08-21 15:28:48 +0200

    Added AutoMortar

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -98,13 +98,13 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	
 	Barrel->Elevate(DeltaRotator.Pitch);
 	//Always yaw the shortest way
-	if (FMath::Abs(DeltaRotator.Yaw) > 180)
+	if (FMath::Abs(DeltaRotator.Yaw) < 180)
 	{
-		Turret->Rotate(-DeltaRotator.Yaw);
+		Turret->Rotate(DeltaRotator.Yaw);
 	}
 	else
 	{
-		Turret->Rotate(DeltaRotator.Yaw);
+		Turret->Rotate(-DeltaRotator.Yaw);
 	}
 }
 
